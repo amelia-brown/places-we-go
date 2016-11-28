fbadda60f2c71755e1766d3a5de9ee9530b1c698
Amelia Brown <amelia.brown389@gmail.com>
2016-11-28 14:06:11 -0800

    (feature): set up react environment, index.html and bundling

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,11 @@
 const webpack = require('webpack');
-
 const path = require('path');
 
 module.exports = {
   entry: path.resolve(__dirname, 'src', 'index.js'),
   output: {
     path: path.resolve(__dirname, 'lib'),
-    fileName: 'bundle.js',
+    filename: 'bundle.js',
   },
   devTool: 'cheap-eval-source-map',
   module: {
@@ -45,15 +44,6 @@ module.exports = {
   },
   plugins: [
     new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-            compress: {
-              warnings: false,
-              screw_ie8: true,
-            },
-            output: {
-              comments: false,
-            },
-            sourcemap: false,
-          }),
+    // TODO: uglify prod build
   ],
 }
